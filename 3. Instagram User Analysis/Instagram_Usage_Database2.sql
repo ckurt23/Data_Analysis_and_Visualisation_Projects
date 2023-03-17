8760e16ba590a1f134859774fb5437340ea3ff0e
Cem Kurt <110932197+ckurt23@users.noreply.github.com>
2023-03-17 18:21:33 +0100

    Create Instagram_Usage_Database2.sql

---
+++ 3. Instagram User Analysis/Instagram_Usage_Database2.sql
@@ -0,0 +1,64 @@
+--Create Database ig_clone in Postgresql
+
+--creating user table
+CREATE TABLE users(
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE PRIMARY KEY,
+	username VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Creating photos table
+CREATE TABLE photos(
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE PRIMARY KEY,
+	image_url VARCHAR(355) NOT NULL,
+	user_id INT NOT NULL,
+	created_dat TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+--comments table creation
+CREATE TABLE comments(
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE PRIMARY KEY,
+	comment_text VARCHAR(255) NOT NULL,
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+--Create Likes table
+CREATE TABLE likes(
+	user_id INT NOT NULL,
+	photo_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	PRIMARY KEY(user_id,photo_id)
+);
+
+--Follows table
+CREATE TABLE follows(
+	follower_id INT NOT NULL,
+	followee_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY (follower_id) REFERENCES users(id),
+	FOREIGN KEY (followee_id) REFERENCES users(id),
+	PRIMARY KEY(follower_id,followee_id)
+);
+
+--Tags table
+CREATE TABLE tags(
+	id INTEGER GENERATED ALWAYS AS IDENTITY UNIQUE PRIMARY KEY,
+	tag_name VARCHAR(255) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+/*junction table: Photos - Tags*/
+CREATE TABLE photo_tags(
+	photo_id INT NOT NULL,
+	tag_id INT NOT NULL,
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	FOREIGN KEY(tag_id) REFERENCES tags(id),
+	PRIMARY KEY(photo_id,tag_id)
+);
