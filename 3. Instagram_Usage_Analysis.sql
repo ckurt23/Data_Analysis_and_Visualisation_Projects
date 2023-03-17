268363fa80064c6cd1a83161e86a8540657fc474
Cem Kurt <110932197+ckurt23@users.noreply.github.com>
2023-03-17 18:17:54 +0100

    Delete 3. Instagram_Usage_Analysis.sql

--- 3. Instagram_Usage_Analysis.sql
+++
@@ -1,6 +0,0 @@
--- We want to reward our users who have been around the longest.  
---Challenge 1: Find the 5 oldest users
-SELECT id, username, NOW()-created_at AS oldest_user_list
-FROM users
-ORDER BY oldest_user_list DESC
-LIMIT 5;
