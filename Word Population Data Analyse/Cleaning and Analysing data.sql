6016da56ef1b89643ba449ec529ee11666e27ef6
Cem Kurt <110932197+ckurt23@users.noreply.github.com>
2023-03-17 17:50:41 +0100

    Update Cleaning and Analysing data.sql

--- Word Population Data Analyse/Cleaning and Analysing data.sql
+++ Word Population Data Analyse/Cleaning and Analysing data.sql
@@ -1,10 +1,9 @@
-This flow includes the work after having downloaded the kaggle dataset and connected the postgres application to the Tableau desktop application.
-The dataset link can be found from the readme file.
+--This flow includes the work after having downloaded the kaggle dataset and connected the postgres application to the Tableau desktop application.
+--The dataset link can be found from the readme file.
 
-------------------------------------------------------------------
 1. Introduction
-------------------------------------------------------------------
-This dataset contains data about the total number of the population at different periods of time between 1970 and 2022.
+
+/*This dataset contains data about the total number of the population at different periods of time between 1970 and 2022.
 The table includes information about;
 the country name, 
 its capitals,
@@ -12,10 +11,10 @@ the continent they are in,
 the total area of the countries,
 the population density for each country,
 the growth rate and the total percentage of each country's population compared to the whole.
-We first create the table naming "world_population" by using the pgadmin interface.
-------------------------------------------------------------------
+We first create the table naming "world_population" by using the pgadmin interface.*/
+
 2. Data Import, Cleaning and Analysis
-------------------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS public."world_population"
 (
     "Rank" integer primary key,
@@ -40,25 +39,21 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public."world_population"
     OWNER to postgres;
     
------------------------------
-At this point, you can proceed to get the data from the file by choosing one among three options. I've chosen to get it with the copy function.
-If face any trouble with file reading authorisation, you need to grant authorisation to everyone for the relevant file that you download from the Kaggle.
 
------------------------------
+/*At this point, you can proceed to get the data from the file by choosing one among three options. I've chosen to get it with the copy function.
+If face any trouble with file reading authorisation, you need to grant authorisation to everyone for the relevant file that you download from the Kaggle.*/
 
 COPY public."world_population"("Rank","CCA3","Country","Capital","Continent","2022_Population","2020_Population","2015_Population","2010_Population","2000_Population","1990_Population","1980_Population","1970_Population","Area_km2","Density_per_km2","Growth_Rate","World_Population_Percentage") 
 FROM 'C:\Users\90537\Downloads\world_population.csv' 
 DELIMITER ',' 
 CSV HEADER;
 
------------------------------
 /* For further action, we take a copy of the main table in the same database. */
 
 SELECT * 
 INTO world_population2
 FROM world_population;
 
------------------------------
 /* Checking for any NULL field */
 
 SELECT * 
@@ -82,29 +77,26 @@ WHERE
     "Growth_Rate" is null OR
     "World_Population_Percentage" is null;
 
------------------------------
 /* See the total number of countries and continents respectively. */
 
 SELECT 
     COUNT(DISTINCT "Country") AS total_country, 
     COUNT(DISTINCT "Continent") AS total_continent
 FROM world_population2;
------------------------------
+
 /* See how many countries each continent covers */
 
 SELECT "Continent", COUNT("Country")
 FROM world_population2
 GROUP BY "Continent"
 ORDER BY COUNT("Country") DESC;
------------------------------
+
 /* See which continent has the most crowded countries in. Half of them are located in Asia. */
 
 SELECT "Country", "2022_Population", "Continent"
 FROM world_population2
 ORDER BY "2022_Population" DESC
 LIMIT 10;
-
------------------------------
 
 /* The total number of population each continent has */
 
@@ -115,8 +107,6 @@ GROUP BY "Continent"
 ORDER BY SUM("2022_Population") DESC;
 
 
------------------------------
-
 /* The countries that are the most crowded and have the highest density per km2 rate. */
 
 SELECT "Rank", "Country", "Density_per_km2", "World_Population_Percentage"
@@ -124,14 +114,13 @@ FROM world_population2
 WHERE "World_Population_Percentage" > 1.5
 ORDER BY "Density_per_km2" DESC;
 
------------------------------
 
-/* For both 2 and 7 year population increase by number difference show that India is in the 1st place with a 10 million and 60 million gap respectively (Between India and Nigeria).
+/* For both 2 and 7 year population increase by number difference show that India is in the 1st place with a 10 million and 60 million gap respectively (Between India and Nigeria).*/
 CREATE TABLE "7_year_gap_by_number" AS
 SELECT "Country","2022_Population" - "2020_Population" AS "2_year_gap", "2022_Population" - "2015_Population" AS "7_year_gap", "Growth_Rate"
 FROM world_population2
 ORDER BY "7_year_gap" DESC;
------------------------------
+
 /* Countries having the highest gap by rate in the population increase */
 
 CREATE TABLE "increase_by_rate_in_52_years" AS
@@ -140,7 +129,6 @@ FROM world_population2
 ORDER BY "gap_by_rate" DESC
 LIMIT 5;
 
------------------------------
 /* The continent total population for each time period */
 
 CREATE TABLE "kita_nüfus_1970_2022" AS
@@ -149,7 +137,7 @@ FROM world_population2
 GROUP BY "Continent"
 ORDER BY "2022_total" DESC;
 
------------------------------
+
 CREATE TABLE "density_population_correlation" AS
 SELECT "Rank","Country", "Density_per_km2"
 FROM world_population2
@@ -157,17 +145,16 @@ ORDER BY "Density_per_km2" DESC
 LIMIT 10;
 
 
------------------------------
-Some important insight from the data:
------------------------------
+/*Some important insight from the data:
+
     - There are total of 234 countries and no missing values for any of fields in the table.
     - The list has information for 6 different continents.
     - There are 8 different columns showing the population number from different years between this period (1970 - 2022).
     - Asia is always at the first place among other continents regarding the total population number.
     - 5 of the all first 10 most crowded countries are from Asia.
-    - United Arab Emirates' population is currently 30 times bigger than its number in 1970, which is the biggest rate of increase among other countries.
-------------------------------------------------------------------
+    - United Arab Emirates population is currently 30 times bigger than its number in 1970, which is the biggest rate of increase among other countries.*/
+
 3. Data Visualisation
-------------------------------------------------------------------
-The rest of the project is to be done and displayed through the link below. The visualisation is completed thanks to the Tableau desktop application.
+
+--The rest of the project is to be done and displayed through the link below. The visualisation is completed thanks to the Tableau desktop application.
 Link: https://public.tableau.com/app/profile/cemkurt/viz/Dashboard_16640559210870/Dashboard1?publish=yes
