c651c123dd5fed365647091a9df144117cd78db0
Cem Kurt <110932197+ckurt23@users.noreply.github.com>
2023-03-19 11:13:08 +0100

    Update Instagram_Usage_Analysis.sql

--- 3. Instagram User Analysis/Instagram_Usage_Analysis.sql
+++ 3. Instagram User Analysis/Instagram_Usage_Analysis.sql
@@ -5,3 +5,14 @@ FROM users
 ORDER BY oldest_user_list DESC
 LIMIT 5;
 
+/*What day of the week do most users register on?
+We need to figure out when to schedule an ad campgain*/
+
+SELECT to_char(created_at, 'Day') AS day_of_week, COUNT(*) AS total_count
+FROM users
+GROUP BY day_of_week
+ORDER BY total_count DESC;
+
+/*We want to target our inactive users with an email campaign.
+Find the users who have never posted a photo*/
+
