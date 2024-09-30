b3313b969449462b01266bd7fc1224bc1b88dc0b
Cem Kurt <110932197+ckurt23@users.noreply.github.com>
2024-09-30 14:31:12 +0200

    Update Day 11.sql

--- 5. 30 days SQL Challenge/Day 11.sql
+++ 5. 30 days SQL Challenge/Day 11.sql
@@ -1,3 +1,4 @@
+/*
 # SQL Case Study: Top 3 Customers by Total Spend
 
 ## Scenario
@@ -20,8 +21,8 @@ Find the **top 3 customers** by the **total amount spent** across all their orde
 3. **Join with Customers Table**: Join the result with the `Customers` table to retrieve customer names.
 4. **Order and Limit**: Order the result by total amount spent in descending order and limit the results to the top 3 customers.
 
-## SQL Query
-```sql
+## SQL Query */
+
 WITH OrderTotals AS (
     SELECT
         O.customer_id,
@@ -43,3 +44,12 @@ GROUP BY
 ORDER BY
     all_amount_of_orders DESC
 LIMIT 3;
+
+/*
+    Explanation
+Step 1: The WITH clause defines a Common Table Expression (CTE) to calculate the total order amount for each line item by multiplying quantity by price_per_unit.
+Step 2: In the main query, we join the result from the CTE with the Customers table using customer_id to retrieve customer names.
+Step 3: We group the results by customer_id and customer_name and calculate the total amount spent per customer using SUM().
+Step 4: Finally, we order the results in descending order by total spend and limit the output to the top 3 customers.
+
+*/
